bff760cbfa0decc83f784756ae8bb24288560734
sethdanny <naddulidaniel1994@gmail.com>
2023-07-14 01:13:48 +0300

    create weighted average

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,24 +1,18 @@
--- Create the stored procedure
-DELIMITER //
-
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+--create proceedure to compute average weighted score for user
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (IN user_id INT)
 BEGIN
-  DECLARE total_score DECIMAL(10, 2);
-  DECLARE total_weight DECIMAL(10, 2);
-  DECLARE weighted_average DECIMAL(10, 2);
+  DECLARE total_score FLOAT;
+  DECLARE total_weight INT;
 
-  -- Calculate the total score and total weight for the user
-  SELECT SUM(c.score * p.weight) INTO total_score, SUM(p.weight) INTO total_weight
-  FROM corrections c
-  JOIN projects p ON c.project_id = p.id
-  WHERE c.user_id = user_id;
+  SELECT SUM(score * weight) INTO total_score
+  FROM corrections
+  WHERE user_id = user_id;
 
-  -- Calculate the weighted average
-  SET weighted_average = total_score / total_weight;
+  SELECT SUM(weight) INTO total_weight
+  FROM projects;
 
-  -- Update the average_score column in the users table
-  UPDATE users SET average_score = weighted_average WHERE id = user_id;
-END //
-
-DELIMITER ;
+  UPDATE users
+  SET average_score = total_score / total_weight
+  WHERE id = user_id;
+END;
 
