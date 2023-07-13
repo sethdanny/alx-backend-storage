858a5ed2914cff36b900cb7e1e2778da0a1d67b5
sethdanny <naddulidaniel1994@gmail.com>
2023-07-14 01:17:44 +0300

    average score

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,60 +1,33 @@
 -- Create the stored procedure
-DELIMITER //
-
-CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers ()
 BEGIN
-  DECLARE user_cursor CURSOR FOR SELECT id FROM users;
-  DECLARE current_user_id INT;
+  DECLARE total_score FLOAT;
+  DECLARE total_weight INT;
+  DECLARE user_id INT;
 
-  -- Declare variables for calculations
-  DECLARE total_score DECIMAL(10, 2);
-  DECLARE total_weight DECIMAL(10, 2);
-  DECLARE weighted_average DECIMAL(10, 2);
+  SELECT COUNT(*) INTO user_id
+  FROM users;
 
-  -- Declare temporary table to hold results
-  CREATE TEMPORARY TABLE IF NOT EXISTS temp_average_scores (
-    user_id INT,
-    average_score DECIMAL(10, 2)
-  );
+  -- Initialize total score and total weight
+  SET total_score = 0;
+  SET total_weight = 0;
 
-  -- Open the cursor
-  OPEN user_cursor;
+  -- Iterate over all users
+  FOR i IN 1..user_id
+  DO
+    -- Retrieve the total score for the user
+    SELECT SUM(score * weight) INTO total_score
+    FROM corrections
+    WHERE user_id = i;
 
-  -- Loop through each user
-  user_loop: LOOP
-    -- Fetch the next user id
-    FETCH user_cursor INTO current_user_id;
+    -- Retrieve the total weight of all the projects
+    SELECT SUM(weight) INTO total_weight
+    FROM projects;
 
-    -- Exit loop if no more users
-    IF current_user_id IS NULL THEN
-      LEAVE user_loop;
-    END IF;
-
-    -- Calculate the total score and total weight for the user
-    SELECT SUM(c.score * p.weight) INTO total_score, SUM(p.weight) INTO total_weight
-    FROM corrections c
-    JOIN projects p ON c.project_id = p.id
-    WHERE c.user_id = current_user_id;
-
-    -- Calculate the weighted average
-    SET weighted_average = total_score / total_weight;
-
-    -- Insert the user's average weighted score into the temporary table
-    INSERT INTO temp_average_scores (user_id, average_score)
-    VALUES (current_user_id, weighted_average);
-  END LOOP;
-
-  -- Close the cursor
-  CLOSE user_cursor;
-
-  -- Update the average_score column in the users table
-  UPDATE users u
-  JOIN temp_average_scores t ON u.id = t.user_id
-  SET u.average_score = t.average_score;
-
-  -- Drop the temporary table
-  DROP TEMPORARY TABLE IF EXISTS temp_average_scores;
-END //
-
-DELIMITER ;
+    -- Update the user's average score
+    UPDATE users
+    SET average_score = total_score / total_weight
+    WHERE id = i;
+  END FOR;
+END;
 
