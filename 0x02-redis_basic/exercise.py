4d2f43e5c7d23e87e7b31203f6eded4445819c56
sethdanny <naddulidaniel1994@gmail.com>
2023-07-19 13:01:09 +0300

    defined get stir and int and converted them back

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -17,3 +17,20 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+        """ Retrieves a value from a Redis value storage """
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn is not None:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """ retrieves a string value from a redis value storage"""
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """ Retrieves an integer value from a Redis value storage"""
+        return self.get(key, fn=int)
